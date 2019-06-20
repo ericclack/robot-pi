552f2521e8e987fa5ce70e676affd1f921cf4ed0
Eric Clack <eric@bn7.net>
2019-06-20 11:11:51 +0100

    Better nav

--- navigate.py
+++ navigate.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 
 from robot import *
+import random
+from time import sleep
 
 # Move about and avoid collisions
 
 try:
     while True:
-        forwards(0.1)
+        forwards(0.05)
+	sleep(0.05)
 
         d=get_distance()
         if d < 25 :
             #we want to turn one way or the other
             turn=random.choice([turn_left,turn_right])
             while d < 25:
-                turn(0.2)
+                turn(0.1)
+                sleep(0.2)
                 d=get_distance()
 
 except KeyboardInterrupt:
