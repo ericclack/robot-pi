025361a2c60192207a41aef9b4138f54cb2daed8
Eric Clack <eric@bn7.net>
2019-11-11 16:03:58 +0000

    Added

---
+++ explore.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from robot import *
+import random
+from time import sleep
+
+distances = [] # An empty list
+
+try:
+    for i in range(30):
+        d=get_distance()
+        distances.append(d)
+        turn_right(0.05)
+        sleep(0.2)
+    for d in distances:
+        print "%.2f" % d
+
+except KeyboardInterrupt:
+       print "Cleaning up"
+       stop()
+       cleanup()
