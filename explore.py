6ec39ab926d38e11536603d2df32d94b5e8ebea7
Eric Clack <eric@bn7.net>
2019-11-11 16:46:20 +0000

    More mapping

--- explore.py
+++ explore.py
@@ -5,15 +5,34 @@ import random
 from time import sleep
 
 distances = [] # An empty list
+threshold = 100
+void = 1500
+counter = 0
+
+# Battery? 0.05 - Power 0.1
+turn_time = 0.09
+
+def check_void(counter):
+    if counter>0:
+        print "void size is", counter
 
 try:
     for i in range(30):
         d=get_distance()
+        if d>=threshold:
+            d=void
         distances.append(d)
-        turn_right(0.05)
+        turn_right(turn_time)
         sleep(0.2)
     for d in distances:
         print "%.2f" % d
+        if d==void:
+            counter+=1
+        else:
+            check_void(counter)
+            counter=0
+    check_void(counter)
+
 
 except KeyboardInterrupt:
        print "Cleaning up"
