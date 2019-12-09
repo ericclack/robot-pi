39bcc819c32950b0b7d83db8ee3494da7f2d794c
Eric Clack <eric@bn7.net>
2019-12-09 15:56:04 +0000

    Move towards biggest void

--- explore.py
+++ explore.py
@@ -7,19 +7,24 @@ from time import sleep
 distances = [] # An empty list
 voids = []
 
-threshold = 150
+threshold = 100
 void = 1500
 
 # Battery? 0.05 - Power 0.1
-turn_time = 0.04
+turn_time = 0.038
 steps=30
 
 def check_void(counter, start_void):
     if counter>0:
-        print "void size is", counter
         voids.append((start_void, counter))
 
+def clear_lists():
+    global distances, voids
+    distances = []
+    voids = []
+
 def scan():
+    clear_lists()
     for i in range(steps):
         d=get_distance()
         if d>=threshold:
@@ -27,13 +32,13 @@ def scan():
         distances.append(d)
         turn_right(turn_time)
         sleep(0.2)
+    print distances
 
 def do_voids():
     i = 0
     counter = 0
     start_void = 0
     for d in distances:
-        print "%.2f" % d
         if d==void:
             if counter == 0:
                 # Beginning of void
@@ -57,20 +62,29 @@ def biggest_void():
     return max_start,max_size
 
 def turn_to_void():
-    start,size=biggest_void()
-    steps_back=steps-start
-    print "step back",steps_back
-    for i in range(steps_back):
-        turn_left(turn_time)
-        sleep(0.2)
+    steps_forward,size=biggest_void()
+    steps_back=steps-steps_forward
+    print "turning to biggest void, back(left)", steps_back, "forward(right)", steps_forward
+    if steps_forward<steps_back:
+        for i in range(steps_forward + size/2):
+            turn_right(turn_time)
+            sleep(0.2)
+    else:
+        for i in range(steps_back - size/2):
+            turn_left(turn_time)
+            sleep(0.2)
 
 try:
-    scan()
-    do_voids()
-    turn_to_void()
+    while True:
+        scan()
+        do_voids()
+        for start, size in voids:
+            print "Void starting at ", start, " size ", size
 
-    for start, size in voids:
-        print "Void starting at ", start, " size ", size
+        turn_to_void()
+
+        while get_distance()>50:
+            forwards(0.1)
 
 except KeyboardInterrupt:
        print "Cleaning up"
